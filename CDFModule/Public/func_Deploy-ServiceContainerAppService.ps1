21f9e036c9f0dcae7fa019b4c122e3b3cd946803
Andreas Stenlund <andreas.stenlund@techseed.se>
2024-09-13 13:02:18 +0200

    Update container environment properties for build context.

--- CDFModule/Public/func_Deploy-ServiceContainerAppService.ps1
+++ CDFModule/Public/func_Deploy-ServiceContainerAppService.ps1
@@ -73,110 +73,6 @@
     Write-Host "appServiceRG: $appServiceRG"
     Write-Host "appServiceName: $appServiceName"
 
-    # #--------------------------------------
-    # # Configure connections for target env
-    # #--------------------------------------
-    # Write-Host "Preparing connections."
-    # $connections = Get-Content -Raw "$InputPath/connections.json" | ConvertFrom-Json -AsHashtable
-
-    # # TODO: Make these configurable using a "platform services" definition file
-    # # Proposal for definition.json:
-    # #   {
-    # #     ServiceBus: [
-    # #       {
-    # #         "Name": "PlatformServiceBus",
-    # #         "ServiceProvider": "servicebus",
-    # #         "Scope": "Platform
-    # #       }
-    # #     ],
-    # #     KeyVault: [
-    # #       {
-    # #         "Name": "PlatformKeyVault",
-    # #         "ServiceProvider": "keyvault",
-    # #         "Scope": "Platform
-    # #       },
-    # #       {
-    # #         "Name": "ApplicationKeyVault",
-    # #         "ServiceProvider": "keyvault",
-    # #         "Scope": "Application
-    # #       },
-    # #       {
-    # #         "Name": "DomainKeyVault",
-    # #         "ServiceProvider": "keyvault",
-    # #         "Scope": "Domain
-    # #       }
-    # #     ]
-    # #   }
-
-
-    # # Platform
-    # ($CdfConfig.Platform.Features.enableKeyVault && Add-LogicAppServiceProviderConnection `
-    #     -connections $connections -ConnectionName "PlatformKeyVault" `
-    #     -serviceProvider "keyvault"`
-    # ) | Out-Null
-
-    # ($CdfConfig.Platform.Features.enableServiceBus && Add-LogicAppServiceProviderConnection `
-    #     -connections $connections `
-    #     -ConnectionName "PlatformServiceBus" `
-    #     -serviceProvider "servicebus" `
-    # ) | Out-Null
-
-    # if ( $CdfConfig.Platform.Features.enableStorageAccount) {
-    #     Add-LogicAppServiceProviderConnection `
-    #         -connections $connections -ConnectionName "PlatformStorageAccountBlob" `
-    #         -serviceProvider "AzureBlob"
-    #     Add-LogicAppServiceProviderConnection `
-    #         -connections $connections -ConnectionName "PlatformStorageAccountFile" `
-    #         -serviceProvider "azurefile"
-    #     Add-LogicAppServiceProviderConnection `
-    #         -connections $connections -ConnectionName "PlatformStorageAccountQueues" `
-    #         -serviceProvider "azurequeues"
-    #     Add-LogicAppServiceProviderConnection `
-    #         -connections $connections -ConnectionName "PlatformStorageAccountTables" `
-    #         -serviceProvider "azureTables"
-    # }
-
-    # # Application
-    # ($CdfConfig.Application.Features.enableKeyVault && Add-LogicAppServiceProviderConnection `
-    #     -connections $connections `
-    #     -ConnectionName "ApplicationKeyVault" `
-    #     -serviceProvider "keyvault" `
-    # ) | Out-Null
-    # ($CdfConfig.Application.Features.enableSftpStorageAccount && Add-LogicAppServiceProviderConnection `
-    #     -connections $connections `
-    #     -ConnectionName "AppSftpStorageAccountBlob" `
-    #     -serviceProvider "AzureBlob" `
-    # ) | Out-Null
-
-    # # Domain
-    # ($CdfConfig.Domain.Features.enableKeyVault && Add-LogicAppServiceProviderConnection `
-    #     -connections $connections `
-    #     -ConnectionName "DomainKeyVault" `
-    #     -serviceProvider "keyvault" `
-    # ) | Out-Null
-
-    # if ( $CdfConfig.Domain.Features.enableStorageAccount) {
-    #     Add-LogicAppServiceProviderConnection `
-    #         -connections $connections `
-    #         -ConnectionName "DomainStorageAccountBlob" `
-    #         -serviceProvider "AzureBlob"
-    #     Add-LogicAppServiceProviderConnection `
-    #         -connections $connections `
-    #         -ConnectionName "DomainStorageAccountFile"`
-    #         -serviceProvider "azurefile"
-    #     Add-LogicAppServiceProviderConnection `
-    #         -connections $connections `
-    #         -ConnectionName "DomainStorageAccountQueues" `
-    #         -serviceProvider "azurequeues"
-    #     Add-LogicAppServiceProviderConnection `
-    #         -connections $connections `
-    #         -ConnectionName "DomainStorageAccountTables" `
-    #         -serviceProvider "azureTables"
-    # }
-
-    # Write-Debug "Connections: $($connections | ConvertTo-Json -Depth 10 | Out-String)"
-    # $connections | ConvertTo-Json -Depth 10 | Set-Content -Path "$OutputPath/connections.json"
-
     #--------------------------------------
     # Preparing appsettings for target env
     #--------------------------------------
@@ -197,108 +93,6 @@
         $updateSettings[$setting.Name] = $setting.Value
     }
 
-    # # TODO: Make these configurable using a "platform services" definition file
-
-    # # Platform Connections Uri Settings
-    # ($CdfConfig.Platform.Features.enableKeyVault && Add-LogicAppAppSettings `
-    #     -SubscriptionId $CdfConfig.Platform.Env.subscriptionId `
-    #     -Config $CdfConfig.Platform `
-    #     -Settings $updateSettings `
-    #     -ConnectionName "PlatformKeyVault" `
-    #     -ParameterName "platformKeyVault" `
-    #     -ServiceProvider "keyvault" `
-    # ) | Out-Null
-
-    # ($CdfConfig.Platform.Features.enableServiceBus && Add-LogicAppAppSettings `
-    #     -SubscriptionId $CdfConfig.Platform.Env.subscriptionId `
-    #     -Config $CdfConfig.Platform -Settings $updateSettings `
-    #     -ConnectionName "PlatformServiceBus" `
-    #     -ParameterName "platformServiceBus" `
-    #     -ServiceProvider "servicebus" `
-    # ) | Out-Null
-
-    # if ( $CdfConfig.Platform.Features.enableStorageAccount) {
-    #     Add-LogicAppAppSettings `
-    #         -SubscriptionId $CdfConfig.Platform.Env.subscriptionId `
-    #         -Config $CdfConfig.Platform -Settings $updateSettings `
-    #         -ConnectionName "PlatformStorageAccountBlob" `
-    #         -ParameterName "platformStorageAccount" `
-    #         -ServiceProvider "AzureBlob"
-    #     Add-LogicAppAppSettings `
-    #         -SubscriptionId $CdfConfig.Platform.Env.subscriptionId `
-    #         -Config $CdfConfig.Platform -Settings $updateSettings `
-    #         -ConnectionName "PlatformStorageAccountFile" `
-    #         -ParameterName "platformStorageAccount" `
-    #         -ServiceProvider "azurefile"
-    #     Add-LogicAppAppSettings `
-    #         -SubscriptionId $CdfConfig.Platform.Env.subscriptionId `
-    #         -Config $CdfConfig.Platform -Settings $updateSettings `
-    #         -ConnectionName "PlatformStorageAccountQueues" `
-    #         -ParameterName "platformStorageAccount" `
-    #         -ServiceProvider "azurequeues"
-    #     Add-LogicAppAppSettings `
-    #         -SubscriptionId $CdfConfig.Platform.Env.subscriptionId `
-    #         -Config $CdfConfig.Platform -Settings $updateSettings `
-    #         -ConnectionName "PlatformStorageAccountTables" `
-    #         -ParameterName "platformStorageAccount" `
-    #         -ServiceProvider "azureTables"
-    # }
-
-    # # Application Connections Uri Settings
-    # ($CdfConfig.Application.Features.enableStorageAccount && Add-LogicAppAppSettings `
-    #     -SubscriptionId $CdfConfig.Platform.Env.subscriptionId `
-    #     -Config $CdfConfig.Application  -Settings $updateSettings `
-    #     -ConnectionName "ApplicationKeyVault" `
-    #     -ParameterName "applicationKeyVault"`
-    #     -ServiceProvider "keyvault" `
-    # ) | Out-Null
-    # ($CdfConfig.Application.Features.enableSftpStorageAccount && Add-LogicAppAppSettings `
-    #     -SubscriptionId $CdfConfig.Platform.Env.subscriptionId `
-    #     -Config $CdfConfig.Application  -Settings $updateSettings `
-    #     -ConnectionName "AppSftpStorageAccountBlob" `
-    #     -ParameterName "appSftpStorageAccount" `
-    #     -ServiceProvider "AzureBlob" `
-    # ) | Out-Null
-
-    # # Domain Connections Uri Settings
-    # ($CdfConfig.Domain.Features.enableKeyVault && Add-LogicAppAppSettings  `
-    #     -SubscriptionId $CdfConfig.Platform.Env.subscriptionId  `
-    #     -Config $CdfConfig.Domain -Settings $updateSettings  `
-    #     -ConnectionName "DomainKeyVault"  `
-    #     -ParameterName "domainKeyVault"  `
-    #     -ServiceProvider "keyvault" `
-    # ) | Out-Null
-    # if ( $CdfConfig.Domain.Features.enableStorageAccount) {
-    #     Add-LogicAppAppSettings  `
-    #         -SubscriptionId $CdfConfig.Platform.Env.subscriptionId  `
-    #         -Config $CdfConfig.Domain  `
-    #         -Settings $updateSettings  `
-    #         -ConnectionName "DomainStorageAccountBlob"  `
-    #         -ParameterName "domainStorageAccount"  `
-    #         -ServiceProvider "AzureBlob"
-    #     Add-LogicAppAppSettings  `
-    #         -SubscriptionId $CdfConfig.Platform.Env.subscriptionId  `
-    #         -Config $CdfConfig.Domain -Settings $updateSettings  `
-    #         -ConnectionName "DomainStorageAccountFile"  `
-    #         -ParameterName "domainStorageAccount"  `
-    #         -ServiceProvider "azurefile"
-    #     Add-LogicAppAppSettings  `
-    #         -SubscriptionId $CdfConfig.Platform.Env.subscriptionId  `
-    #         -Config $CdfConfig.Domain `
-    #         -Settings $updateSettings  `
-    #         -ConnectionName "DomainStorageAccountQueues"  `
-    #         -ParameterName "domainStorageAccount"  `
-    #         -ServiceProvider "azurequeues"
-    #     Add-LogicAppAppSettings  `
-    #         -SubscriptionId $CdfConfig.Platform.Env.subscriptionId  `
-    #         -Config $CdfConfig.Domain  `
-    #         -Settings $updateSettings  `
-    #         -ConnectionName "DomainStorageAccountTables"  `
-    #         -ParameterName "domainStorageAccount"  `
-    #         -ServiceProvider "azureTables"
-    # }
-
-
     # Get service config from cdf-config.json
     $serviceConfig = Get-Content -Path "$InputPath/cdf-config.json" | ConvertFrom-Json -AsHashtable
 
@@ -317,24 +111,11 @@
 
             }
             "Secret" {
-                # $secret = Get-AzKeyVaultSecret `
-                #     -DefaultProfile $azCtx `
-                #     -VaultName $CdfConfig.Domain.ResourceNames.keyVaultName `
-                #     -Name "svc-$($CdfConfig.Service.Config.serviceName)-$($setting.Identifier)" `
-                #     -AsPlainText `
-                #     -ErrorAction SilentlyContinue
-
-                # if ($null -eq $secret) {
-                #     Write-Warning " KeyVault secret for Identifier [$($setting.Identifier)] not found"
-                #     Write-Warning " Expecting secret name [svc-$($CdfConfig.Service.Config.serviceName)-$($setting.Identifier)] in Domain KeyVault"
-                # }
-                # else {
                 $appSettingRef = "@Microsoft.KeyVault(VaultName=$($CdfConfig.Domain.ResourceNames.keyVaultName );SecretName=svc-$($CdfConfig.Service.Config.serviceName)-$($setting.Identifier))"
                 $appSettingKey = "Param_$serviceSettingKey"
                 $updateSettings[$appSettingKey] = $appSettingRef
                 $Parameters.Service.value[$setting.Identifier] = "@appsetting('$appSettingKey')"
                 Write-Verbose "Prepared KeyVault secret reference for Setting [$($setting.Identifier)] using app setting [$appSettingKey] KeyVault ref [$appSettingRef]"
-                # }
             }
         }
     }
@@ -388,17 +169,23 @@
     foreach ($hostName in $app.HostNames) { $BaseUrls += "https://$hostName" }
     $updateSettings["SERVICE_API_BASEURLS"] = $BaseUrls | Join-String -Separator ','
 
-    if ($CdfConfig.Application.Env.purpose -eq 'production') {
-        $updateSettings["WEBSITE_RUN_FROM_PACKAGE"] = "1"
-        Write-Host "PRODUCTION: Enable 'WEBSITE_RUN_FROM_PACKAGE' which prevents editing in Azure Portal." -ForegroundColor Yellow
-    }
-    else {
-        $updateSettings["WEBSITE_RUN_FROM_PACKAGE"] = "0"
-        Write-Host "NON-PRODUCTION: Disabling 'WEBSITE_RUN_FROM_PACKAGE' which allows editing in Azure Portal." -ForegroundColor Blue
-    }
     #-------------------------------------------------------------
     # Preparing the app settings
     #-------------------------------------------------------------
+
+    # Service Identity
+    $updateSettings["SERVICE_NAME"] = $CdfConfig.Service.Config.serviceName
+    $updateSettings["SERVICE_TYPE"] = $CdfConfig.Service.Config.serviceType
+    $updateSettings["SERVICE_GROUP"] = $CdfConfig.Service.Config.serviceGroup
+    $updateSettings["SERVICE_TEMPLATE"] = $CdfConfig.Service.Config.serviceTemplate
+    $updateSettings["DOMAIN_NAME"] = $CdfConfig.Domain.Config.domainName
+
+    # Build information
+    $updateSettings["BUILD_COMMIT"] = $env:GITHUB_SHA ?? $env:BUILD_SOURCEVERSION ?? $(git -C $TemplateDir rev-parse --short HEAD)
+    $updateSettings["BUILD_RUN"] = $env:GITHUB_RUN_ID ?? $env:BUILD_BUILDNUMBER ?? "local"
+    $updateSettings["BUILD_BRANCH"] = $env:GITHUB_REF_NAME ?? $env:BUILD_SOURCEBRANCH ?? $(git -C $TemplateDir branch --show-current)
+    $updateSettings["BUILD_REPOSITORY"] = $env:GITHUB_REPOSITORY ?? $env:BUILD_REPOSITORY_NAME ?? $(Split-Path -Leaf (git -C $TemplateDir remote get-url origin))
+    $updateSettings["BUILD_PIPELINE"] = $env:GITHUB_WORKFLOW_REF ?? $env:BUILD_DEFINITIONNAME ?? "local"
 
     $updateSettings | ConvertTo-Json -Depth 10 | Set-Content -Path "$OutputPath/app.settings.gen.json"
 
@@ -425,17 +212,6 @@
     #--------------------------------------
     # Deploy container app implementation
     #--------------------------------------
-    Write-Host "Deploying workflows."
-
-    Compress-Archive -Force  `
-        -Path "$OutputPath/*"  `
-        -DestinationPath "$OutputPath/deployment-package-$($CdfConfig.Service.Config.serviceName).zip"
-
-    Publish-AzWebApp -Force `
-        -Name $appServiceName `
-        -ResourceGroupName $appServiceRG `
-        -ArchivePath "$OutputPath/deployment-package-$($CdfConfig.Service.Config.serviceName).zip" `
-        -WarningAction:SilentlyContinue | Out-Null
 
     Write-Host "Container App Service implementation deployment done."
 }
