1928ad06a37bf4aefde7859770f76c54b29bca7c
AN-Epical <abhishek.nehra@epicalgroup.com>
2024-09-16 14:15:01 +0200

    Functions for importing GitHub secrets into Key vault (#3)
    * Added functions for importing secrets
    
    * Updated description

---
+++ CDFModule/Public/func_Import-GitHubSecretsToKeyVault.ps1
@@ -0,0 +1,81 @@
+Function Import-GitHubSecretsToKeyVault {
+    <#
+    .SYNOPSIS
+
+    Imports a set of secrets from GitHub into a target key vault.
+
+    .DESCRIPTION
+    The command takes 3 inputs:
+    - JSON (As Hashtable) of all configured GitHub secrets.
+      Must have the following format:
+      {
+        "Secret1": "somevalue",
+        "Secret2": "somevalue",
+        "Secret3": "somevalue"
+      }
+    - path of the file having list of GitHub secrets to be imported and respective key name to be used in KV.
+      The file must have the following JSON format:
+      [
+        {
+          "kvSecretName": "Secret-1",
+          "ghSecretName": "Secret1"
+        },
+        {
+          "kvSecretName": "Secret-3",
+          "ghSecretName": "Secret3"
+        }
+      ]
+    - KeyVault where secrets has to be imported.    
+
+    .PARAMETER GithubSecrets
+    GitHub Secrets as HashTable
+
+    .PARAMETER GithubKeyVaultMappingFilePath
+    File Path
+
+    .PARAMETER KeyVaultName
+    The name of the target key vault.
+
+    .INPUTS
+    None. You cannot pipe objects.
+
+    .OUTPUTS
+    None.
+
+    .EXAMPLE
+    PS> Import-CdfGitHubSecretsToKeyVault -GithubSecrets "Github secrets json as hashtable" `
+        -GithubKeyVaultMappingFilePath "FilePath" -KeyVaultName "KeyVaultName"
+
+    .LINK
+    
+    #>
+
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory = $true)]
+        [hashtable] $GithubSecrets,    
+        [Parameter(Mandatory = $true)]
+        [string] $GithubKeyVaultMappingFilePath,
+        [Parameter(Mandatory = $true)]
+        [string] $KeyVaultName        
+    )    
+            if (Test-Path $GithubKeyVaultMappingFilePath) {
+              $ghKvList = Get-Content  $GithubKeyVaultMappingFilePath | ConvertFrom-Json -AsHashtable
+              $secretsList = @()
+              foreach($ghKvItem in $ghKvList) {
+                foreach($ghSecret in $GithubSecrets.Keys) {
+                  if($ghKvItem.ghSecretName -eq $ghSecret){
+                    $keyValue = @{
+                      kvSecretName = $ghKvItem.kvSecretName
+                      kvValue = $GithubSecrets[$ghSecret]}   
+                    $secretsList += $keyValue
+                  }
+                }
+              }              
+              $secretsList | Import-KeyVaultSecrets -Name $KeyVaultName
+            }
+            else {
+                Write-Host "No secrets needed to be imported from GitHub to KeyVault"
+            }
+
+}
---
+++ CDFModule/Public/func_Import-KeyVaultSecrets.ps1
@@ -0,0 +1,70 @@
+Function Import-KeyVaultSecrets {
+    <#
+    .SYNOPSIS
+
+    Imports a set of secrets from JSON file input into a target key vault
+
+    .DESCRIPTION
+
+    The command takesan array of secrets to import. 
+    The secrets JSON must have the following format.
+
+    [
+        {
+            "kvSecretName": "Secret-1",
+            "kvValue": "somevalue"
+        },
+        {
+            "kvSecretName": "Secret-2",
+            "kvValue": "somevalue"
+        },
+        {
+            "kvSecretName": "Secret-3",
+            "kvValue": "somevalue"
+        }
+    ]
+
+    .PARAMETER Name
+    The name of the target key vault.
+
+    .INPUTS
+    Secrets
+
+    .OUTPUTS
+    None.
+
+    .EXAMPLE
+    PS> Import-CdfGitHubSecretsToKeyVault ... | Import-CdfKeyVaultSecrets `
+            -Name "KeyVault Name"
+            
+
+    PS> (Get-Content  "secrets json file path" | ConvertFrom-Json -AsHashtable) `
+            | Import-CdfKeyVaultSecrets `
+            -Name "KeyVault Name" 
+
+    .LINK
+    Import-CdfGitHubSecretsToKeyVault
+
+    #>
+
+    [CmdletBinding()]
+    Param(
+        [Parameter(ValueFromPipeline = $true, Mandatory = $true)]
+        [hashtable] $Secrets,    
+        [Parameter(Mandatory = $true)]
+        [string] $Name        
+    )
+    
+    foreach ($Secret in $Secrets) {
+        Write-Verbose "Processing $($Secret.kvSecretName)"
+        $CurrentSecret = Get-AzKeyVaultSecret -VaultName $Name -Name $Secret.kvSecretName -AsPlainText 
+        if($Secret.kvValue  -eq $CurrentSecret) {
+            Write-Verbose " - Existing, match, no change"
+        } else {
+            Write-Verbose " - Add/Update"
+            $SecretValue = ConvertTo-SecureString $Secret.kvValue -AsPlainText -Force 
+            Set-AzKeyVaultSecret -VaultName $Name -Name $Secret.kvSecretName -SecretValue $SecretValue
+        }
+    }
+
+}
