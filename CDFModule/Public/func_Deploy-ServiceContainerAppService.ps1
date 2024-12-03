b63bf73d753757d530d1d61a5abd12b9ee3fad53
Andreas Stenlund <andreas.stenlund@techseed.se>
2024-12-03 16:46:39 +0100

    Update AppService container configuration to make use of new standard environment variables

--- CDFModule/Public/func_Deploy-ServiceContainerAppService.ps1
+++ CDFModule/Public/func_Deploy-ServiceContainerAppService.ps1
@@ -61,10 +61,11 @@
 
     $azCtx = Get-AzureContext -SubscriptionId $CdfConfig.Platform.Env.subscriptionId
 
-    # Copy service implementation
+    # Copy service config
     $containerFiles = @(
         'cdf-config.json',
-        'cdf-secrets.json'
+        'cdf-secrets.json',
+        'app.settings.json'
     )
     Copy-Item -Force -Recurse -Include $containerFiles -Path $InputPath/* -Destination $OutputPath
 
@@ -89,150 +90,55 @@
 
     $appSettings = $app.SiteConfig.AppSettings
 
-    # Preparing hashtable with exsting config
+    # Preparing hashtable with existing app config
     $updateSettings = ConvertFrom-Json -InputObject "{}" -AsHashtable
     foreach ($setting in $appSettings) {
         $updateSettings[$setting.Name] = $setting.Value
     }
 
-    # Get service config from cdf-config.json
-    $cdfConfigFile = Join-Path -Path $InputPath  -ChildPath 'cdf-config.json'
-    $cdfSchemaPath = Join-Path -Path $MyInvocation.MyCommand.Module.ModuleBase -ChildPath 'Resources/Schemas/cdf-service-config.schema.json'
-    if (!(Test-Json -SchemaFile $cdfSchemaPath -Path $cdfConfigFile)) {
-        Write-Error "Service configuration file did not validate. Please check errors above and correct."
-        Write-Error "File path:  $cdfConfigFile"
-        return
-    }
-    $serviceConfig = Get-Content -Raw $cdfConfigFile | ConvertFrom-Json -AsHashtable
+    # Update with CDF Standard settings
+    $updateSettings = $CdfConfig `
+    | Get-ServiceConfigSettings `
+        -UpdateSettings $updateSettings `
+        -InputPath $InputPath `
+        -ErrorAction:Stop
 
+    # Configure service API URLs
+    $updateSettings["SVC_API_BASEURL"] = "https://$($app.HostNames[0])"
+    $BaseUrls = @()
+    foreach ($hostName in $app.HostNames) { $BaseUrls += "https://$hostName" }
+    $updateSettings["SVC_API_BASEURLS"] = [string]($BaseUrls | Join-String -Separator ',')
 
-    # Service internal settings
-    foreach ($serviceSettingKey in $serviceConfig.ServiceSettings.Keys) {
-        Write-Host "Adding service internal setting: $serviceSettingKey"
-        $setting = $serviceConfig.ServiceSettings[$serviceSettingKey]
-        switch ($setting.Type) {
-            "Constant" {
-                $updateSettings["SVC_$serviceSettingKey"] = ($setting.Value | Out-String -NoNewline)
-            }
-            "Setting" {
-                $updateSettings["SVC_$serviceSettingKey"] = ($setting.Values[0].Value | Out-String -NoNewline)
+    $acrName = $cdfConfig.Application.ResourceNames.containerRegistryName
+    $cdfEnvId = $cdfConfig.Application.Env.definitionId
+    $cdfDomainName = $cdfConfig.Domain.Config.domainName
+    $cdfServiceName = $CdfConfig.Service.Config.serviceName
+    $imageTag = $CdfConfig.Service.Config.imageTag ?? 'latest'
+    $imageName = "$acrName.azurecr.io/$cdfEnvId/$cdfDomainName/$cdfServiceName"
 
-            }
-            "Secret" {
-                $secret = Get-AzKeyVaultSecret `
-                    -DefaultProfile $azCtx `
-                    -VaultName $CdfConfig.Domain.ResourceNames.keyVaultName `
-                    -Name "svc-$($CdfConfig.Service.Config.serviceName)-$($setting.Identifier)" `
-                    -ErrorAction SilentlyContinue
+    $updateSettings["CDF_IMAGE_NAME"] = $imageName
+    $updateSettings["CDF_IMAGE_TAG"] = $imageTag
 
-                if ($null -eq $secret) {
-                    Write-Warning " KeyVault secret for Identifier [$($setting.Identifier)] not found in KeyVault"
-                    Write-Warning " Expecting secret name [svc-$($CdfConfig.Service.Config.serviceName)-$($setting.Identifier)] in Domain KeyVault"
-                }
-                $appSettingRef = "@Microsoft.KeyVault(VaultName=$($CdfConfig.Domain.ResourceNames.keyVaultName );SecretName=svc-$($CdfConfig.Service.Config.serviceName)-$($setting.Identifier))"
-                $appSettingKey = "Param_$serviceSettingKey"
-                $updateSettings[$appSettingKey] = $appSettingRef
-                Write-Verbose "Prepared KeyVault secret reference for Setting [$($setting.Identifier)] using app setting [$appSettingKey] KeyVault ref [$appSettingRef]"
-            }
-        }
-    }
-
-    # Service external settings
-    foreach ($externalSettingKey in $serviceConfig.ExternalSettings.Keys) {
-        Write-Host "Adding service external setting: $externalSettingKey"
-        $setting = $serviceConfig.ExternalSettings[$externalSettingKey]
-        switch ($setting.Type) {
-            "Constant" {
-                $updateSettings["EXT_$externalSettingKey"] = ($setting.Value | Out-String -NoNewline)
-
-            }
-            "Setting" {
-                [string] $value = ($setting.Values  | Where-Object { $_.Purpose -eq $CdfConfig.Application.Env.purpose }).Value
-                $updateSettings["EXT_$externalSettingKey"] = $value
-            }
-            "Secret" {
-                $secret = Get-AzKeyVaultSecret `
-                    -DefaultProfile $azCtx `
-                    -VaultName $CdfConfig.Domain.ResourceNames.keyVaultName `
-                    -Name "svc-$($CdfConfig.Service.Config.serviceName)-$($setting.Identifier)" `
-                    -ErrorAction SilentlyContinue
-
-                if ($null -eq $secret) {
-                    Write-Warning " KeyVault secret for Identifier [$($setting.Identifier)] not found"
-                    Write-Warning " Expecting secret name [svc-$($CdfConfig.Service.Config.serviceName)-$($setting.Identifier)] in Domain KeyVault"
-                }
-
-                $appSettingRef = "@Microsoft.KeyVault(VaultName=$($CdfConfig.Domain.ResourceNames.keyVaultName );SecretName=svc-$($CdfConfig.Service.Config.serviceName)-$($setting.Identifier))"
-                $appSettingKey = "EXT_$externalSettingKey"
-                $updateSettings[$appSettingKey] = $appSettingRef
-                Write-Verbose "Prepared KeyVault secret reference for Setting [$($setting.Identifier)] using app setting [$appSettingKey] KeyVault ref [$appSettingRef]"
-            }
-        }
-    }
-
-    if ($null -ne $updateSettings["SVC_API_BASEURL"]) {
-        # Configure service API URLs for the App Service
-        $updateSettings["SVC_API_BASEURL"] = "https://$($app.HostNames[0])"
-        $BaseUrls = @()
-        foreach ($hostName in $app.HostNames) { $BaseUrls += "https://$hostName" }
-        $updateSettings["SVC_API_BASEURLS"] = $BaseUrls | Join-String -Separator ','
-    }
-
-    #-------------------------------------------------------------
-    # Preparing the app settings
-    #-------------------------------------------------------------
-
-    # CDF Env details
-    $updateSettings["CDF_ENV_DEFINITION_ID"] = $CdfConfig.Application.Env.definitionId
-    $updateSettings["CDF_ENV_NAME_ID"] = $CdfConfig.Application.Env.nameId
-    $updateSettings["CDF_ENV_NAME"] = $CdfConfig.Application.Env.name
-    $updateSettings["CDF_ENV_SHORT_NAME"] = $CdfConfig.Application.Env.shortName
-    $updateSettings["CDF_ENV_DESCRIPTION"] = $CdfConfig.Application.Env.description
-    $updateSettings["CDF_ENV_PURPOSE"] = $CdfConfig.Application.Env.purpose
-    $updateSettings["CDF_ENV_QUALITY"] = $CdfConfig.Application.Env.quality
-    $updateSettings["CDF_ENV_REGION_CODE"] = $CdfConfig.Application.Env.regionCode
-    $updateSettings["CDF_ENV_REGION_NAME"] = $CdfConfig.Application.Env.regionName
-
-    # Service Identity
-    $updateSettings["CDF_SERVICE_NAME"] = $CdfConfig.Service.Config.serviceName
-    $updateSettings["CDF_SERVICE_TYPE"] = $CdfConfig.Service.Config.serviceType
-    $updateSettings["CDF_SERVICE_GROUP"] = $CdfConfig.Service.Config.serviceGroup
-    $updateSettings["CDF_SERVICE_TEMPLATE"] = $CdfConfig.Service.Config.serviceTemplate
-    $updateSettings["CDF_DOMAIN_NAME"] = $CdfConfig.Domain.Config.domainName
-
-    # Build information
-    $updateSettings["CDF_BUILD_COMMIT"] = $env:GITHUB_SHA ?? $env:BUILD_SOURCEVERSION ?? $(git -C $TemplateDir rev-parse --short HEAD)
-    $updateSettings["CDF_BUILD_RUN"] = $env:GITHUB_RUN_ID ?? $env:BUILD_BUILDNUMBER ?? "local"
-    $updateSettings["CDF_BUILD_BRANCH"] = $env:GITHUB_REF_NAME ?? $env:BUILD_SOURCEBRANCH ?? $(git -C $TemplateDir branch --show-current)
-    $updateSettings["CDF_BUILD_REPOSITORY"] = $env:GITHUB_REPOSITORY ?? $env:BUILD_REPOSITORY_NAME ?? $(Split-Path -Leaf (git -C $TemplateDir remote get-url origin))
-    $updateSettings["CDF_BUILD_PIPELINE"] = $env:GITHUB_WORKFLOW_REF ?? $env:BUILD_DEFINITIONNAME ?? "local"
-    $updateSettings["CDF_BUILD_BRANCH"] = $env:GITHUB_REF_NAME ?? $env:BUILD_SOURCEBRANCH ?? $(git -C $TemplateDir branch --show-current)
-
-    $updateSettings | ConvertTo-Json -Depth 10 | Set-Content -Path "$OutputPath/app.settings.gen.json"
-
-    # Substitute Tokens in the app.settings file
-    $tokenValues = $CdfConfig | Get-TokenValues
-    Update-ConfigFileTokens `
-        -InputFile "$OutputPath/app.settings.gen.json" `
-        -OutputFile "$OutputPath/app.settings.json" `
-        -Tokens $tokenValues `
-        -StartTokenPattern '{{' `
-        -EndTokenPattern '}}' `
-        -NoWarning `
-        -WarningAction:SilentlyContinue
-
-    # Read generated app.settings file with token substitutions
-    $updateSettings = Get-Content -Path "$OutputPath/app.settings.json" | ConvertFrom-Json -Depth 10 -AsHashtable
-
+    #--------------------------------------
+    # Deploy app configuration
+    #--------------------------------------
     Set-AzWebApp `
         -Name $appServiceName `
         -ResourceGroupName $appServiceRG `
         -AppSettings $updateSettings `
         -WarningAction:SilentlyContinue | Out-Null
 
-    #--------------------------------------
-    # Deploy container app implementation
-    #--------------------------------------
+    $webConfig = Get-AzResource  -DefaultProfile $azCtx -Id "$($app.Id)/config" -ExpandProperties
+    $webConfig.Properties.linuxFxVersion
+    $webConfig.Properties.linuxFxVersion = "DOCKER|${imageName}:${imageTag}"
+    $webConfig.Properties.numberOfWorkers = $CdfConfig.Service.Config.numberOfWorkers ?? 1
+    $webConfig | Set-AzResource -DefaultProfile $azCtx -Force | Out-Null
 
-    Write-Host "Container App Service implementation deployment done."
+    Restart-AzWebApp `
+        -SoftRestart `
+        -Name $appServiceName `
+        -ResourceGroupName $appServiceRG `
+        -WarningAction:SilentlyContinue | Out-Null
+
+    Write-Host "Container App Service deployment done."
 }
